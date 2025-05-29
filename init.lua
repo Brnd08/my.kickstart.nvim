de1d8eb7495645859c116c3af46869620ff843b1
Brnd08 <93061195+Brnd08@users.noreply.github.com>
2025-05-29 17:06:51 -0600

    Add oil.nvim plugin to vim config
    Add oil.nvim via lazynvim along with configurations from repository

--- init.lua
+++ init.lua
@@ -478,6 +478,64 @@ require('lazy').setup({
       },
     },
   },
+
+  -- Buffer like filesystem tree
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+          local m = name:match '^%.'
+          return m ~= nil
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+          return false
+        end,
+        -- Sort file names with numbers in a more intuitive order for humans.
+        -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+        natural_order = 'fast',
+        -- Sort file and directory names case insensitive
+        case_insensitive = false,
+        sort = {
+          -- sort order can be "asc" or "desc"
+          -- see :help oil-columns to see which columns are sortable
+          { 'type', 'asc' },
+          { 'name', 'asc' },
+        },
+        -- Customize the highlight group for the file name
+        highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+          return nil
+        end,
+      },
+      -- EXPERIMENTAL support for performing file operations with git
+      git = {
+        -- Return true to automatically git add/mv/rm files
+        add = function(path)
+          return false
+        end,
+        mv = function(src_path, dest_path)
+          return false
+        end,
+        rm = function(path)
+          return false
+        end,
+      },
+    },
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
